5535bc6962d987b957bddbd5485acd5430dfc511
Chris Hamons <chris.hamons@xamarin.com>
2020-06-02 10:42:02 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace virens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# virens
+
+A report generator for Github builds that how long since we've had a "fully green" build.
+
+## Definition
+
+- (of plants) green
+- flourishing; vigorous
